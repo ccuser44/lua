2bddc1a76b1ea5a902794e620e87efb74bf1f1a9
ccuser44 <68124053+ccuser44@users.noreply.github.com>
2026-07-16 11:54:31 +0300

    Allow `xpcall` to have multiple arguments

--- src/lbaselib.cpp
+++ src/lbaselib.cpp
@@ -393,9 +393,10 @@ static int luaB_pcall (lua_State *L) {
 static int luaB_xpcall (lua_State *L) {
   int status;
   luaL_checkany(L, 2);
-  lua_settop(L, 2);
-  lua_insert(L, 1);  /* put error function under function to be called */
-  status = lua_pcall(L, 0, LUA_MULTRET, 1);
+  lua_pushvalue(L, 1);  /* exchange function... */
+  lua_copy(L, 2, 1);  /* ...and error handler */
+  lua_replace(L, 2);  /* put error function under function to be called */
+  status = lua_pcall(L, lua_gettop(L) - 2, LUA_MULTRET, 1);
   lua_pushboolean(L, (status == 0));
   lua_replace(L, 1);
   return lua_gettop(L);  /* return status + all results */
